a9d99a82b3d77cec20ffde2d87b9700d679870c2
Rich Lewis <rich@alternatefuture.uk>
2022-05-12 21:22:15 +0100

    Changed full name & added apostrophe script

--- chapter02/full_name.py
+++ chapter02/full_name.py
@@ -1,4 +1,8 @@
+from email import message
+
+
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
-print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
