58a0e14139de15a3c344aeacefb3b2be0addc353
Rich Lewis <rich@alternatefuture.uk>
2022-05-18 22:10:34 +0100

    First set of exercises in chapter 3

---
+++ chapter03/exercise3_2/greetings.py
@@ -0,0 +1,4 @@
+friends = ['Jody', 'Ross', 'Rob']
+print(f"Good morrow, {friends[0]}!")
+print(f"Good morrow, {friends[1]}!")
+print(f"Good morrow, {friends[2]}!")
---
+++ chapter03/exercise3_3/my_own_list.py
@@ -0,0 +1,3 @@
+games = ['agricola', 'a feast for odin', 'great western trail', 'beyond the sun']
+print(f"It's a tricky one. Should I play {games[0].title()} or {games[1].title()}?")
+print(f"Or how about {games[-1].title()} or {games[-2].title()}?")
